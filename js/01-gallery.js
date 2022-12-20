c16c3056d64cee7c4f18f93c8c9d893bd2fc64f6
Anton-Oxxom <anton.shchetynin@gmail.com>
2022-12-20 15:12:16 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryImg = galleryItems.map((element) => 
+  
+`<div class="gallery__item">
+<a class="gallery__link" href="${element.original}">
+<img
+    class="gallery__image"
+    src="${element.preview}"
+    data-source= "${element.original}"
+    alt="${element.description}"
+/>
+</a>
+</div>`).join("");
+
+galleryEl.innerHTML = galleryImg;
+
+galleryEl.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const imgAttribute = event.target.getAttribute("data-source");
+
+   const showImg = basicLightbox.create(`
+    <img src="${imgAttribute}" />
+`, {
+});
+    
+showImg.show();
+});
+
