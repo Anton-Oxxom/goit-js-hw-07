e64d2f715a4e26d63d442a95705e660ec37f1541
Anton-Oxxom <anton.shchetynin@gmail.com>
2022-12-25 20:12:00 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryImg = galleryItems.map((element) => 
+  
+`<div class="gallery__item">
+<a class="gallery__link" href="${element.original}">
+<img
+    class="gallery__image"
+    src="${element.preview}"
+    data-source= "${element.original}"
+    alt="${element.description}"
+/>
+</a>
+</div>`).join("");
+
+galleryEl.innerHTML = galleryImg;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,});
